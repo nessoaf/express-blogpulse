d45ec8c2df6ee78a961f980fbd388610b72e8146
nessoaf <64707980+nessoaf@users.noreply.github.com>
2020-06-25 20:01:02 -0700

    love you pete

--- controllers/articles.js
+++ controllers/articles.js
@@ -3,59 +3,59 @@ var db = require('../models')
 var router = express.Router()
 
 // POST /articles - create a new post
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
   db.article.create({
     title: req.body.title,
     content: req.body.content,
     authorId: req.body.authorId
   })
-  .then(function(post) {
-    res.redirect('/')
-  })
-  .catch(function(error) {
-    res.status(400).render('main/404')
-  })
+    .then(function (post) {
+      res.redirect('/')
+    })
+    .catch(function (error) {
+      res.status(400).render('main/404')
+    })
 })
 
 // GET /articles/new - display form for creating new articles
-router.get('/new', function(req, res) {
+router.get('/new', function (req, res) {
   db.author.findAll()
-  .then(function(authors) {
-    res.render('articles/new', { authors: authors })
-  })
-  .catch(function(error) {
-    res.status(400).render('main/404')
-  })
+    .then(function (authors) {
+      res.render('articles/new', { authors: authors })
+    })
+    .catch(function (error) {
+      res.status(400).render('main/404')
+    })
 })
 
 // GET /articles/:id - display a specific post and its author
-router.get('/:id', function(req, res) {
+router.get('/:id', function (req, res) {
   db.article.findOne({
     where: { id: req.params.id },
-    include: [db.author, db.comments]
+    include: [db.author, db.comment]
   })
-  .then(function(article) {
-    if (!article) throw Error()
-    console.log(article.author)
+    .then(function (article) {
+      if (!article) throw Error()
+      console.log(article.author)
 
-    res.render('articles/show', { article: article, id: req.params.id })
-  })
-  .catch(function(error) {
+      res.render('articles/show', { article: article })
+    })
+    .catch(function (error) {
+      console.log(error)
+      res.status(400).render('main/404')
+    })
+})
+
+router.post('/:id/comments', (req, res) => {
+  db.comment.create({
+    name: req.body.authorName,
+    content: req.body.authorComment,
+    articleId: req.params.id
+  }).then((post) => {
+    res.redirect(`/articles/${req.params.id}`)
+  }).catch(function (error) {
     console.log(error)
     res.status(400).render('main/404')
   })
 })
-
-router.post('/:id', (req,req) => {
-  db.comment.create({
-    name: req.body.name,
-    content: req.body.content,
-    articleId: req.params.id
-  }).then((post) => {
-    res.redirect(`/articles/${req.params.id}`)
-  }).catch(function(error) {
-    console.log(error)
-    res.status(400).render('main/404')
-  })
-}))
 module.exports = router
--- models/comment.js
+++ models/comment.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const comment = sequelize.define('comment', {
+    //object
     name: DataTypes.STRING,
     content: DataTypes.TEXT,
     articleId: DataTypes.INTEGER
